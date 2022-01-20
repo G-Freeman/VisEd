b58d9ec3edd53e7f8da998ff048afb7263e64c66
Freeman <dr.zukis@rambler.ru>
2022-01-20 17:00:51 +0300

    Grid, generator rework, start zoom btns, zoom unlimited math

--- components/Grid.vue
+++ components/Grid.vue
@@ -3,11 +3,16 @@
 		:id="`grid-${id}`"
 		@wheel = "grid_onMouseWheel"
 	>
-		<div class="grid_debug" v-if="isDev">
-			<label v-text="`Small Lines: ${hSmallGridLineCount} x ${vSmallGridLineCount}`"></label>
-			<label v-text="`Big: ${hBigGridLineCount} x ${vBigGridLineCount}`"></label>
-			<label v-text="`Lines: ${gridLinesSumm}, Quads: ${gridLinesMulti}`"></label>
-			<label v-text="`Zoom: ${zoom.value.toFixed(1)}`"></label>
+		<div class="grid_tools">
+			<div class="zoom">
+				<div class="zoom_preset" v-for="i in 3" v-text="i"/>
+			</div>
+			<div class="grid_debug" v-if="isDev">
+				<label v-text="`Small Lines: ${hSmallGridLineCount} x ${vSmallGridLineCount}`"></label>
+				<label v-text="`Big: ${hBigGridLineCount} x ${vBigGridLineCount}`"></label>
+				<label v-text="`Lines: ${gridLinesSumm}, Quads: ${gridLinesMulti}`"></label>
+				<label v-text="`Zoom: ${zoom.value.toFixed(1)}`"></label>
+			</div>
 		</div>
 		<svg class="grid_svg" height="100%" width="100%">
 			<!-- Back -->
@@ -15,26 +20,20 @@
 			<!-- Small -->
 			<template v-if="isInited">
 				<line v-for="(i,num) in hSmallGridLineCount" :key="`hs${num}`" v-if="defer(1)"
-					x1="0" :y1="(num * (smallGridSpacing*zoom.value))+1"
-					x2="100%" :y2="(num * (smallGridSpacing*zoom.value))+1"
-					:style="`stroke:${smallGridColor}`"
+					x1="0"		:y1="(num * ((smallGridSpacing / (2**Math.floor(zoom.value)||1)) * (2**zoom.value) ))+1"
+					x2="100%"	:y2="(num * ((smallGridSpacing / (2**Math.floor(zoom.value)||1)) * (2**zoom.value) ))+1"
+					:style="`
+						stroke:${num%8 ? smallGridColor : bigGridColor}
+					`"
 				/>
 				<line v-for="(i,num) in vSmallGridLineCount" :key="`vs${num}`" v-if="defer(2)"
-					:x1="(num * (smallGridSpacing*zoom.value))+1" y1="0"
-					:x2="(num * (smallGridSpacing*zoom.value))+1" y2="100%"
-					:style="`stroke:${smallGridColor}`"
+					:x1="(num * ((smallGridSpacing / (2**Math.floor(zoom.value)||1)) * (2**zoom.value) ))+1" y1="0"
+					:x2="(num * ((smallGridSpacing / (2**Math.floor(zoom.value)||1)) * (2**zoom.value) ))+1" y2="100%"
+					:style="`
+						stroke:${num%8 ? smallGridColor : bigGridColor};
+					`"
 				/>
-				<!-- Big -->
-				<line v-for="(i,num) in hBigGridLineCount" :key="`hb${num}`" v-if="defer(3)"
-					x1="0" :y1="(num * (bigGridSpacing*zoom.value))+1"
-					x2="100%" :y2="(num * (bigGridSpacing*zoom.value))+1"
-					:style="`stroke:${bigGridColor}`"
-				/>
-				<line v-for="(i,num) in vBigGridLineCount" :key="`vb${num}`" v-if="defer(4)"
-					:x1="(num * (bigGridSpacing*zoom.value))+1" y1="0"
-					:x2="(num * (bigGridSpacing*zoom.value))+1" y2="100%"
-					:style="`stroke:${bigGridColor}`"
-				/>
+
 			</template>
 		</svg>
 		<selectRect/>
@@ -56,15 +55,15 @@ export default {
 		Defer()
 	],
 	props:{
-		id:				{default: (()=>{ return 0 })()	},
-		smallGridColor: {default:'#353535'	},
-		bigGridColor:	{default:'#151515'	},
+		id:				{default: '0'		},
+		smallGridColor: {default: '#353535'	},
+		bigGridColor:	{default: '#151515'	},
 	},
 	data: function () {
 		return {
 			isInited: false,
 
-			bigGridSpacing: 200,
+			bigGridSpacing: 400,
 			smallGridSpacing: 25,
 
 			hSmallGridLineCount: 0,
@@ -74,6 +73,9 @@ export default {
 
 			zoom: {
 				value: 1,
+				step: .1,
+				min: 1,
+				max: 32
 			},
 
 			gridObj: null,
@@ -108,21 +110,16 @@ export default {
 			this.redraw();
 		},
 		redraw() {
-			this.vSmallGridLineCount = Math.ceil(this.gridObj.offsetWidth/(this.smallGridSpacing*this.zoom.value));
-			this.hSmallGridLineCount = Math.ceil(this.gridObj.offsetHeight/(this.smallGridSpacing*this.zoom.value));
-
-			this.vBigGridLineCount = Math.ceil(this.gridObj.offsetWidth/(this.bigGridSpacing*this.zoom.value));
-			this.hBigGridLineCount = Math.ceil(this.gridObj.offsetHeight/(this.bigGridSpacing*this.zoom.value));
+			this.vSmallGridLineCount = Math.ceil(this.gridObj.offsetWidth /(this.smallGridSpacing * (Math.floor(this.zoom.value)||1) )) * ((Math.floor(this.zoom.value)||1));
+			this.hSmallGridLineCount = Math.ceil(this.gridObj.offsetHeight/(this.smallGridSpacing * (Math.floor(this.zoom.value)||1) )) * ((Math.floor(this.zoom.value)||1));
+			this.vBigGridLineCount	 = Math.ceil(this.gridObj.offsetWidth /(this.bigGridSpacing	 * (Math.floor(this.zoom.value)||1) )) * ((Math.floor(this.zoom.value)||1));
+			this.hBigGridLineCount	 = Math.ceil(this.gridObj.offsetHeight/(this.bigGridSpacing	 * (Math.floor(this.zoom.value)||1) )) * ((Math.floor(this.zoom.value)||1));
 		},
 		grid_onMouseWheel(wheel) {
-			if(this.zoom.value>=.2 && wheel.deltaY<0) {
-				this.zoom.value += .1
-			}
-			if(this.zoom.value<=3 && wheel.deltaY>0) {
-				this.zoom.value -= .1;
-			}
-			if(this.zoom.value<.2) this.zoom.value=.2;
-			if(this.zoom.value>3)  this.zoom.value=3;
+			if(this.zoom.value >= this.zoom.min && wheel.deltaY < 0) { this.zoom.value += this.zoom.step; }
+			if(this.zoom.value <= this.zoom.max && wheel.deltaY > 0) { this.zoom.value -= this.zoom.step; }
+			if(this.zoom.value <  this.zoom.min) this.zoom.value = this.zoom.min;
+			if(this.zoom.value >  this.zoom.max) this.zoom.value = this.zoom.max;
 
 			this.redraw();
 		},
@@ -140,19 +137,45 @@ export default {
 		overflow: hidden;
 		//border: 8px crimson solid;
 		margin: 12px;
-		&_debug {
+		&_tools {
 			position: absolute;
-			top: 12px;
-			left: 12px;
-			background: #00000040;
-			color: gray;
-			//padding: 4px;
+			top: 0;
+			pointer-events: all;
+			.zoom {
+				display: flex;
+				border-radius: 4px;
+				background: #00000040;
+				padding: 2px;
+				margin: 12px;
+				border: 1px solid rgba(0, 0, 0, 0.25);
+				&_preset {
+					width: 12px;
+					padding: 4px;
+					color: #c48922;
+					background: rgba(0, 0, 0, 0.25);
+					margin: 2px;
+					text-align: center;
+					border: 1px solid rgba(0, 0, 0, 0.25);
+					&:hover {
+						background: rgba(40, 39, 39, 0.25);
+						cursor: pointer;
+					}
+				}
+			}
+
+		}
+		&_debug {
+			margin: 12px;
+			background: rgba(0, 0, 0, 0.25);
+			color: #9ca181;
 			display: flex;
+			padding: 4px;
 			flex-direction: column;
 			border-radius: 4px;
 			z-index: 999;
 			user-select: none;
 			pointer-events: none;
+			border: 1px solid rgba(0, 0, 0, 0.25);
 		}
 		&_svg {
 
